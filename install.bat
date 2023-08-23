9590de3b6c45dcdea07df6087fc7fb0afb88cc6f
Mister Maximus <92948927+wiseman-timelord@users.noreply.github.com>
2023-08-23 10:49:13 +0100

    Fixed.

--- install.bat
+++ install.bat
@@ -1,11 +1,6 @@
 @echo off
 cls
 
-:: If the script receives a directory argument, set it as the current directory
-if not "%~1"=="" (
-    cd /d %~1
-)
-
 :: Store the current directory
 set "currentDir=%cd%"
 echo Current working directory detected: %currentDir%
@@ -22,8 +17,12 @@ if %errorLevel% == 0 (
 echo Requesting administrative privileges...
 echo.
 
+:: Create a temporary batch file to set the directory
+echo @echo off > "%temp%\tempBatch.bat"
+echo cd /d "%currentDir%" >> "%temp%\tempBatch.bat"
+echo "%~f0" >> "%temp%\tempBatch.bat"
+
 :: Get the full path of the current batch script
-setlocal
 set "batchPath=%~f0"
 echo Batch script path: %batchPath%
 goto checkUAC
@@ -31,15 +30,11 @@ goto checkUAC
 :checkUAC
 echo Creating VBScript to request admin privileges...
 echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-echo UAC.ShellExecute "%batchPath%", "%currentDir%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%temp%\tempBatch.bat", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
 "%temp%\getadmin.vbs"
 exit /b
 
 :gotAdmin
-:: Set the working directory back to the original location
-cd /d "%currentDir%"
-echo Set working directory back to: %currentDir%
-
 :: If running with admin privileges, proceed with the script
 echo Installing the requirements...
 echo.
@@ -47,7 +42,7 @@ pip install -r requirements.txt
 
 :: Check if requirements.txt was found and installed successfully
 if %errorLevel% == 0 (
-    echo Requirements install finised.
+    echo Requirements install finished.
 ) else (
     echo ERROR: Could not install.
 )
